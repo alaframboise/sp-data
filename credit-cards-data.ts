f451c815905f63a715a2c5deaf79a9e3b21bc745
John Paxton <pax@speedingplanet.com>
2017-08-02 09:43:36 -0400

    Initial commit of CC data

---
+++ credit-cards-data.ts
@@ -0,0 +1,24 @@
+let creditCards = [
+  {
+    "ccId"  : 0,
+    "ccType": "basic",
+    "ccName": "Basic Card"
+  },
+  {
+    "ccId"  : 1,
+    "ccType": "copper",
+    "ccName": "Copper Card"
+  },
+  {
+    "ccId"  : 2,
+    "ccType": "silver",
+    "ccName": "Silver Card"
+  },
+  {
+    "ccId"  : 3,
+    "ccType": "gold",
+    "ccName": "Gold Card"
+  }
+];
+
+export {creditCards};
